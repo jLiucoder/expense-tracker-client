e0784a845c974ee07583724095d044a544c4ffdf
jLiucoder <55926282+jLiucoder@users.noreply.github.com>
2024-02-07 15:26:15 -0500

    expriment form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ExpenseForm from "./form";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function App() {
+  const [total, setTotal] = useState(0);
+  const [totalList, setTotalList] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          `http://${process.env.REACT_APP_HOST_IP}:8080/total`
+        );
+        const list = await axios.get(
+          `http://${process.env.REACT_APP_HOST_IP}:8080/totalList`
+        );
+        setTotal(res.data[0].total);
+        setTotalList(list.data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const deleteItem = async (index) => {
+    try {
+      console.log(index);
+      const res = await axios.delete(
+        `http://${process.env.REACT_APP_HOST_IP}:8080/`,
+        {
+          data: { index: index },
+        }
+      );
+      console.log("res", res);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <small>
+          You are running this application in{" "}
+          <b>{process.env.REACT_APP_NODE_ENV}</b> mode.
+        </small>
+        <h1>Expense Tracker</h1>
+        <p>Total amount: ${total == undefined ? 0 : total}</p>
+        <h3>Expense List</h3>
+        <div style={{ height: "300px", overflowY: "scroll" }}>
+          <div>
+            {totalList.map((item) => (
+              <span key={item.index}>
+                <p>
+                  {item.item} : {item.amount}{" "}
+                  <DeleteIcon onClick={() => deleteItem(item.index)} />
+                </p>
+              </span>
+            ))}
+          </div>
+        </div>
+
+        <ExpenseForm />
       </header>
     </div>
   );
---
+++ src/form.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import axios from "axios";
+function ExpenseForm() {
+  const [formData, setFormData] = useState({ item: "", amount: "" });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can perform any action with formData here, e.g., send it to the server
+    const postData = async () => {
+      try {
+        const res = await axios.post("http://localhost:8080/", formData);
+        console.log("res", res);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    postData();
+    console.log(formData);
+    // Reset the form fields
+    setFormData({ item: "", amount: "" });
+  };
+
+  return (
+    <div>
+      <h3>Expense Form</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="item">Item:</label>
+          <input
+            type="text"
+            id="item"
+            name="item"
+            value={formData.item}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="amount">Amount:</label>
+          <input
+            type="number"
+            id="amount"
+            name="amount"
+            value={formData.amount}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default ExpenseForm;
